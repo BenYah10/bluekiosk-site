e7fc795db9df2f4a9fbb2e75cb754b26f2dbf8bb
Ben Yahmed Mohamed <benyahmed.mohamed17@gmail.com>
2025-08-26 20:09:43 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -179,7 +179,7 @@ const EMAILJS_CONFIG = {
 };
 
 // 1) Mets ici l'URL /exec de ton déploiement Apps Script
-const SHEET_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxesBmREwAxAlgq18QiAi7NvEdVEfGW2ltn8xCqxeEmyA35xG7h0n2oMjqGR1LWYZoU/exec';
+const SHEET_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxzXRqitqhbltmxJXd6O8StVc4fCzqUmQMG6R4M4EInttia4jRl-ZSw-poII6-MGod0/exec';
 
 // 2) Construit l'objet avec EXACTEMENT les clés attendues par Code.gs
 function buildSheetParams(fd) {
